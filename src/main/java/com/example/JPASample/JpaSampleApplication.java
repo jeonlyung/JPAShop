0a66d33798f72091f4fb52c81a9354f2f211caa7
HA SANG JAE <bindongx2@naver.com>
2022-04-29 11:45:43 +0900

    빌드 및 Lombok셋팅 테스트 확인

--- src/main/java/com/example/JPASample/JpaSampleApplication.java
+++ src/main/java/com/example/JPASample/JpaSampleApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaSampleApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+
+
 		SpringApplication.run(JpaSampleApplication.class, args);
+
 	}
 
 }
