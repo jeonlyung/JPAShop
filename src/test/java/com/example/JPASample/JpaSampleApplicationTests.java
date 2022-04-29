c2817ba02761b5591a6e40a0380bdabf6ed97643
HA SANG JAE <bindongx2@naver.com>
2022-04-29 09:31:41 +0900

    Initial commit

---
+++ src/test/java/com/example/JPASample/JpaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPASample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
