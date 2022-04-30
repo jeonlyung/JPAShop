a778359abf17cf2acfa92df195c35718fef353ae
HA SANG JAE <bindongx2@naver.com>
2022-04-30 14:54:58 +0900

    데이터베이스(H2)설치 및 프로젝트 연결  - DB연결 마무리  - 외부 라이브러리 추가 및 yml 설정값 추가(org.hibernate.type)

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -16,7 +16,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional           /* 트랜잭션 어노테이션(스프링) */
-    @Rollback(value = false) /* false일 경우 롤백 안하고 커밋 바로 진행함 */
+    @Rollback(value = false) /* false일 경우 TEST 롤백 안하고 커밋 바로 진행함 */
     public void testMember() throws Exception {
         //given
         Member member = new Member();
@@ -29,6 +29,10 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertEquals(findMember.getId(), member.getId());
         Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+        Assertions.assertEquals(findMember, member);
+        
+        //1차 캐시에서 기존에 관리하던 엔티티 가져옴(이론)
+        System.out.println("findeMember == member : " + (findMember == member));
     }
 
 }
