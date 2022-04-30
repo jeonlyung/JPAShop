f4aec3a896894b2872e98b1653f47d21a4d7e2dd
HA SANG JAE <bindongx2@naver.com>
2022-04-30 14:30:42 +0900

    데이터베이스(H2)설치 및 프로젝트 연결  - DB연결 테스트 코드 작성 및 확인

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional           /* 트랜잭션 어노테이션(스프링) */
+    @Rollback(value = false) /* false일 경우 롤백 안하고 커밋 바로 진행함 */
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+    }
+
+}
