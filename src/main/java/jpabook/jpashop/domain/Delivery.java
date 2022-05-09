c35fb1b9e1ffdc0a29f87f07df07feb690a9152b
하상재 <bindongx2@naver.com>
2022-05-09 17:57:42 +0900

    Delivery Table 엔티티 개발 및 연동

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,4 +1,25 @@
 package jpabook.jpashop.domain;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
 public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded //내장타입
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // ORDINAL Default (ORDINAL 쓰면 중간에 추가 못하므로 쓰지말고 String 쓰자)
+    private DeliveryStatus status; //READY, COMP
 }
