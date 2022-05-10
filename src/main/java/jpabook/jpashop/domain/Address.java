44b6ade88e21441f9feba782ad564995d764825a
하상재 <bindongx2@naver.com>
2022-05-10 14:14:48 +0900

    Address Class 설정 추가     - 기본 생성자 추가(Embedded)     - Setter제공X(값 변경 불가)

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,16 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //기본 생성자 필요(JPA 구현 라이브러리가 객체를 생성할 때 리플랙션 같은 기술을 사용할 수 있도록 지원해야 하기 때문)
+    //JPA 스펙에선 protected 선호
+    protected Address() {
+    }
+
+    //Setter 제공X (값 변경 못하도록 설정)
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
