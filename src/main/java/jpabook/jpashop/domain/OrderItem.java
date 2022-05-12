3716c8f8e15c94411276ccfbb3ec9bce7c5ed198
HA SANG JAE <bindongx2@naver.com>
2022-05-12 18:02:54 +0900

    [주문] 주문 비지니스 로직 및 생성 메소드 추가     - cancel() : 취소 메소드     - getTotalPrice() : 총 주문 가격 메소드

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,27 @@ public class OrderItem {
 
     private int orderPrice;  //주문 가격
     private int count;       //주문 수량
+
+    //==생성 메소드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        //Item 엔티티에 가격 컬럼이 있지만, 할인 및 쿠폰으로 인하여 orderPrice가격이 변동될 수 있으므로 따로 파라미터 가져가는것이 좋다.
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    //취소 메소드
+    public void cancel(){//재고수량 RollBack
+        getItem().addStock(count);
+    }
+
+    //총 주문가격 조회 메소드
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
