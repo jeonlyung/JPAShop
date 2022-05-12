d25555eac5aeba545294aaad356cafdc6b4bfb23
HA SANG JAE <bindongx2@naver.com>
2022-05-12 15:15:05 +0900

    [상품] 상품 Service class 개발     - save(item) : 상품 저장     - findOne(id) : 상품 단건 조회     - findAll() : 상품 전체 조회

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional /* 쓰기 메소드이므로 @Transcational 선언 */
+    public void save(Item item){
+        itemRepository.save(item);
+    }
+
+    //상품 단건 조회
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+    //상품 전체 조회
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+
+}
