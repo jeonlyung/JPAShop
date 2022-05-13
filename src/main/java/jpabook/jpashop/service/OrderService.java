d10de6dd7e9aaadeea2b330852fd4c81e024e8c2
HA SANG JAE <bindongx2@naver.com>
2022-05-13 17:59:38 +0900

    [주문] 주문 Service 생성 및 메소드 추가  - order(id,id,count) : 주문 메소드  - cancelOrder(id) : 주문 취소 메소드  - findOrders(orderSearch) : 주문 검색 메소드

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -62,5 +64,8 @@ public class OrderService {
     /**
      *  주문 검색
      */
-
+    /*
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }*/
 }
