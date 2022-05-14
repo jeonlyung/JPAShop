c051c682ad6acee4ccffbff682c7af0d46f8093f
HA SANG JAE <bindongx2@naver.com>
2022-05-14 11:21:07 +0900

    [주문] 주문 TEST 메소드 추가     - 상품주문() : 주문 테스트     - 상품주문_재고수량초과() : 재고 수량 체크 테스트     - 주문취소() : 주문 취소 메소드
        - Entity 객체 생성 공통 메소드 추가

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,9 +6,9 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,9 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
-import org.junit.jupiter.api.Assertions.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,32 +32,78 @@ public class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         //given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "과천","123-123"));
-        em.persist(member);
+        Member member = getMember();
 
+        String testBookName = "시골JPA";
         int testPrice = 10000;
         int testStockQuantity = 10;
-        Book book = new Book();
-        book.setName("시골 JPA");
-        book.setPrice(testPrice);
-        book.setStockQuantity(testStockQuantity);
-        em.persist(book);
+        Book book = getBook(testBookName, testPrice, testStockQuantity);
+
+        int orderCount = 2;
 
         //when
-        int orderCount = 2;
         Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
 
         //then
         Order getOrder = orderRepository.findOne(orderId);
+
         assertEquals(OrderStatus.ORDER, getOrder.getStatus(), "상품 주문시 상태는 ORDER");
         assertEquals(1, getOrder.getOrderItems().size(), "주문한 상품 종류 수가 정확해야한다.");
         assertEquals(testPrice * orderCount, getOrder.getTotalPrice(), "주문 가격은 가격 * 수량이다.");
         assertEquals(8, book.getStockQuantity(), "주문 수량만큼 재고가 줄어야 한다.");
+    }
 
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        Member member = getMember();
+        Item item = getBook("시골JPA", 10000, 10);
+
+        int orderCount = 11;
+        //when
+        orderService.order(member.getId(), item.getId(), orderCount); //removeStock()에서 Exception발생
+
+        //then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+        Member member = getMember();
+        int testStockQuantity = 10;
+        Item item = getBook("시골 JPA", 10000,testStockQuantity);
+
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals(OrderStatus.CANCEL, getOrder.getStatus(), "주문 취소시 상태는 CANCLEL 이다.");
+        assertEquals(testStockQuantity, item.getStockQuantity(), "주문 취소된 상품은 그만큼 재고가 증가해야 한다.");
 
     }
 
-    
+    //Member 생성 메소드
+    private Member getMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "과천","123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    //Book 생성 메소드
+    private Book getBook(String testBookName, int testPrice, int testStockQuantity) {
+        Book book = new Book();
+        book.setName(testBookName);
+        book.setPrice(testPrice);
+        book.setStockQuantity(testStockQuantity);
+        em.persist(book);
+        return book;
+    }
 }
