dbd8c24df7d83fc1227662a4fe2f4eb7895e153e
HA SANG JAE <bindongx2@naver.com>
2022-05-15 18:14:53 +0900

    불필요한 파일 제거 및 정리

--- src/main/java/jpabook/jpashop/JpaSampleApplication.java
+++ src/main/java/jpabook/jpashop/JpaSampleApplication.java
@@ -1,17 +1,15 @@
 package jpabook.jpashop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JpaSampleApplication {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
-
+		log.info("Main System Start!!");
 		SpringApplication.run(JpaSampleApplication.class, args);
 
 	}
