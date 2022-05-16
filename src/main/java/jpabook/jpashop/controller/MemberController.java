0eec764e8d59a2b4abc15713a1a66a4cd047877b
하상재 <bindongx2@naver.com>
2022-05-16 18:05:07 +0900

    [회원등록] MemberController 추가     - create(form) : 회원 등록 메소드

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return  "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form){
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";  //저장이 되고 다시 재로딩되면 안좋기 떄문에 redirect를 쓴다.
+        //14:12분
+    }
+}
