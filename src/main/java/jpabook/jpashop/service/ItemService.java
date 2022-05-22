d1fa63015095e43c0089016716ef5154056a6794
HA SANG JAE <bindongx2@naver.com>
2022-05-22 11:05:31 +0900

    [상품 수정] Service 변경 감지,merge 예제 메소드 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,32 @@ public class ItemService {
     public void saveItem(Item item){
         itemRepository.save(item);
     }
+
+    /**
+     * 변경 감지 update예제
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        //변경 감지(영속 상태)
+        Item findItem = itemRepository.findOne(itemId); //영속 상태
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        //영속 상태이므로 Transcation 상태에서 변한된 값들을 flush해줌
+    }
+
+    /**
+     * 병합(merge) update예제
+     */
+    @Transactional
+    public Item mergeItem(Long itemId, Book param){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+        return findItem;
+    }
+
 
     //상품 단건 조회
     public Item findOne(Long itemId) {
