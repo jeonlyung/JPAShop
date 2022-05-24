a5625d5b149df897b3230c3b51d45f1f3a9ec030
하상재 <bindongx2@naver.com>
2022-05-24 14:32:46 +0900

    Hibernate5Module 라이브러리 추가     - Bean 소스 추가

--- src/main/java/jpabook/jpashop/JpaSampleApplication.java
+++ src/main/java/jpabook/jpashop/JpaSampleApplication.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
@@ -12,6 +14,15 @@ public class JpaSampleApplication {
 		log.info("Main System Start!!");
 		SpringApplication.run(JpaSampleApplication.class, args);
 
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module(){ //LAZY 로딩 무시
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		/* //LAZY 로딩 다 가져옴
+		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		*/
+		return hibernate5Module;
 	}
 
 }
