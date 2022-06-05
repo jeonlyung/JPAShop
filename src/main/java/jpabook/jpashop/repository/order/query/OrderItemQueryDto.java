79e45d4e6cf85da95549c504bd5687d35e47c32e
HA SANG JAE <bindongx2@naver.com>
2022-06-05 17:52:39 +0900

    [OrderV4 에러] unexpected token: oi

--- src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop.repository.order.query;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class OrderItemQueryDto {
 
+    @JsonIgnore
     private Long orderId;
     private String itemName;
     private int orderPrice;
