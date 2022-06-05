9fecb1370b9c9cc51cb94eaee5582fe755659c49
HA SANG JAE <bindongx2@naver.com>
2022-06-05 17:52:48 +0900

    [OrderV4 에러] unexpected token: oi

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -12,28 +12,33 @@ public class OrderQueryRepository {
 
     private final EntityManager em;
 
+    /**
+     * 컬렉션은 별도로 조회
+     */
     public List<OrderQueryDto> findOrderQueryDtos() {
+        //루트 조회(toOne 코드를 모두 한번에 조회)
         List<OrderQueryDto> result = findOrders();
-
+        //루프를 돌면서 컬렉션 추가(추가 쿼리 실행)
         result.forEach(o -> {
-            List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
+            List<OrderItemQueryDto> orderItems =
+                    findOrderItems(o.getOrderId());
             o.setOrderItems(orderItems);
         });
-
         return result;
     }
 
-    private List<OrderItemQueryDto> findOrderItems(Long orderId) {
+    public List<OrderItemQueryDto> findOrderItems(Long orderId) {
         return em.createQuery(
                 "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
                         " join oi.item i" +
-                        "where oi.order.id = :ordersId", OrderItemQueryDto.class)
+                        "where oi.order.id = :orderId", OrderItemQueryDto.class)
                 .setParameter("orderId", orderId)
                 .getResultList();
     }
 
-    private List<OrderQueryDto> findOrders() {
+
+    public List<OrderQueryDto> findOrders() {
         return em.createQuery(
                         "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
                                 " from Order o" +
