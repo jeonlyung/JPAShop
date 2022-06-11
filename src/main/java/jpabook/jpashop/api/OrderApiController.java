c6566a776b5959428788f0eb31db6312e09d0eb0
HA SANG JAE <bindongx2@naver.com>
2022-06-11 11:42:44 +0900

    [OrderV6 Dto 쿼리 한번으로 조회]      - Controller Method 추가

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
@@ -16,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -50,7 +53,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllDsl(new OrderSearch());
         List<OrderDto> result = orders.stream()
                 .map(order -> new OrderDto(order))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
@@ -63,7 +66,7 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(order -> new OrderDto(order))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
@@ -79,7 +82,7 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(order -> new OrderDto(order))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
@@ -95,8 +98,26 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5(){
         return orderQueryRepository.findAllByDto_optimization();
-
     }
+
+    /**
+     * 쿼리 한번으로 조회
+     * 쿼리는 한번이지만 조인으로 인해 DB에서 애플리케이션에 전달되는 데이터에 중복 데이터가 추가되므로 V5보다 느릴 수 있다.
+     * 애플리케이션에서 추가 작업이 크다 / 페이징처리 불가
+     */
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6(){
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+    }
+
     /**
      * Order Dto
      */
@@ -118,7 +139,7 @@ public class OrderApiController {
             address = order.getDelivery().getAddress();
             orderItems = order.getOrderItems().stream()
                     .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
 
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@EqualsAndHashCode(of = "orderId")
+@EqualsAndHashCode(of = "orderId") //collect group by 할때 기준 설정
 public class OrderQueryDto {
 
     private Long orderId;
@@ -27,4 +27,12 @@ public class OrderQueryDto {
         this.address = address;
     }
 
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
 }
